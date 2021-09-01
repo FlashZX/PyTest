13ac8541aa12f8a20338105f0dd7be9f9caa0698
FlashZX <43445283+FlashZX@users.noreply.github.com>
2021-09-02 03:30:18 +0500

    add "test_items.py" & "conftest.py" files

---
+++ conftest.py
@@ -0,0 +1,23 @@
+'''
+1. Создайте GitHub-репозиторий, в котором будут лежать файлы conftest.py и test_items.py.
+2. Добавьте в файл conftest.py обработчик, который считывает из командной строки параметр language.
+3. Реализуйте в файле conftest.py логику запуска браузера с указанным языком пользователя.
+   Браузер должен объявляться в фикстуре browser и передаваться в тест как параметр.
+'''
+
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default=None,
+                     help='Choose language: ru, en, es, fr')
+
+@pytest.fixture(scope='function')
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+    browser = webdriver.Chrome(options = options)
+    yield browser
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,14 @@
+'''
+4. В файл test_items.py напишите тест, который проверяет, что страница товара на сайте содержит кнопку добавления в корзину.
+   Например, можно проверять товар, доступный по http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/.
+5. Тест должен запускаться с параметром language следующей командой: pytest --language=es test_items.py
+   и проходить успешно. Достаточно, чтобы код работал только для браузера Сhrome.
+'''
+import time
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+def test_check_button(browser):
+    browser.get(link)
+    time.sleep(30)
+    # Используем find_elements что бы не падало с исключением, если элемент не найден.
+    assert browser.find_elements_by_css_selector('.btn-add-to-basket'), 'Элемент не найден!'
